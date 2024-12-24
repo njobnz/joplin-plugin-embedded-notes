61f78ecad6c582af2c07f7e60292d30e44c5fc93
Nelson Job <njobnz@proton.me>
2024-12-24 17:49:45 +1300

    fix: generateEmbeddedNote should use existing replaceTokens function

--- src/modules/generateEmbeddedNote.ts
+++ src/modules/generateEmbeddedNote.ts
@@ -1,29 +1,5 @@
 import { EmbeddableNote } from '../types';
-import escape from '../utils/escapeRegExp';
+import replaceTokens from '../utils/replaceTokens';
 
-export default (text: string, embeddings: Map<string, EmbeddableNote>): string => replaceTokens(text, embeddings);
-
-/**
- * Replaces all tokens in a text string with their corresponding note text.
- *
- * This method doesn't take into account renderer tags, so all tokens are
- * replaced with the raw content from their referenced notes.
- *
- * @param {string} text - The input text.
- * @param {Map<string, EmbeddableNote>} embeddings - A mapping of note titles and ids to their note object.
- * @returns {string} The replaced text.
- */
-const replaceTokens = (text: string, embeddings: Map<string, EmbeddableNote>): string => {
-  if (text) {
-    // Replaces tokens in the order they appear in the string.
-    // For ambiguous tokens, only the first one is replaced.
-    const filter = Array.from(embeddings.keys()).filter(token => text.includes(token));
-    const sorted = filter.sort((a, b) => text.indexOf(a) - text.indexOf(b));
-    sorted.forEach(item => {
-      const embed = embeddings.get(item) ?? null;
-      if (!embed) return;
-      text = text.replace(new RegExp(escape(item), 'g'), embed.note?.body || '');
-    });
-  }
-  return text;
-};
+export default (text: string, embeddings: Map<string, EmbeddableNote>): string =>
+  replaceTokens(text, Object.fromEntries(embeddings));
