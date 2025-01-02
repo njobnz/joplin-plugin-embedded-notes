b6da77cec067736fc30dd02414caba96e74aeef0
Nelson Job <njobnz@proton.me>
2025-01-02 19:23:36 +1300

    fix: only render embedded resources if plugin is enabled
    render embedded pdfs

--- src/plugins/markdownIt/assets/index.ts
+++ src/plugins/markdownIt/assets/index.ts
@@ -3,6 +3,7 @@ import {
   EmbeddedLinksPosition,
   GET_DATA_CMD,
   GET_EMBEDDED_LINKS_CMD,
+  GET_GLOBAL_VALUE_CMD,
   GET_SETTING_CMD,
   MARKDOWNIT_SCRIPT_ID,
 } from '../../../constants';
@@ -26,6 +27,13 @@ export class EmbeddedNotes {
       command: GET_DATA_CMD,
       path,
       query,
+    });
+  }
+
+  async fetchGlobal<T>(name: string): Promise<T> {
+    return await webviewApi.postMessage(MARKDOWNIT_SCRIPT_ID, {
+      command: GET_GLOBAL_VALUE_CMD,
+      name,
     });
   }
 
@@ -72,54 +80,64 @@ export class EmbeddedNotes {
 
   async updateResources(): Promise<void> {
     const elements = document.querySelectorAll('a[href], img[src]') as any;
-    const pattern = /^joplin-content:\/\/note-viewer\/(.*)\/\/([0-9A-Fa-f]{32})$/;
+    const pattern = /^joplin-content:\/\/note-viewer\/(.*)\/\/([0-9A-Fa-f]{32})(|#[^\s]*)(|\s".*?")$/;
 
     for (const element of elements) {
-      const resourceUrl = element.src || element.title;
-      if (!pattern.test(resourceUrl)) continue;
-      const resourceId = resourceUrl.slice(-32);
+      const resourceUrl = element.src || element.href;
+      const match = resourceUrl.match(pattern);
+      if (!match) continue;
+      const resourceId = match[2];
 
-      if (element.tagName === 'IMG') {
-        const { file_extension } = (await this.fetchData(['resources', resourceId], {
-          fields: ['file_extension'],
-        })) as any;
-        if (file_extension) element.src = `${resourceUrl}.${file_extension}?t=${Date.now()}`;
-        continue;
-      }
-
-      if (element.tagName !== 'A') continue;
-
-      const { title, mime, file_extension } = (await this.fetchData(['resources', resourceId], {
-        fields: ['title', 'mime', 'file_extension'],
+      const resource = (await this.fetchData(['resources', resourceId], {
+        fields: element.tagName === 'IMG' ? ['file_extension'] : ['title', 'mime', 'file_extension'],
       })) as any;
 
-      element.setAttribute('data-resource-id', resourceId);
-      element.setAttribute('title', title);
-      element.setAttribute('type', mime);
-      element.setAttribute(
-        'onclick',
-        `ipcProxySendToHost("joplin://${resourceId}", { resourceId: "${resourceId}" }); return false;`
-      );
+      if (element.tagName === 'IMG') {
+        if (resource.file_extension) element.src = `${resourceUrl}.${resource.file_extension}?t=${Date.now()}`;
+      }
 
-      const type = mime.split('/')[0];
-      const icon = mime ? getClassNameForMimeType(mime) : 'fa-joplin';
+      if (element.tagName === 'A') {
+        const { title, mime, file_extension } = resource;
+        element.setAttribute('data-resource-id', resourceId);
+        element.setAttribute('title', title);
+        element.setAttribute('type', mime);
+        element.setAttribute('href', `${element.href}.${file_extension}`);
+        element.setAttribute(
+          'onclick',
+          `ipcProxySendToHost("joplin://${resourceId}", { resourceId: "${resourceId}" }); return false;`
+        );
 
-      const iconEl = document.createElement('span');
-      iconEl.className = `resource-icon ${icon}`;
-      element.prepend(iconEl);
+        const icon = mime ? getClassNameForMimeType(mime) : 'fa-joplin';
+        const iconEl = document.createElement('span');
+        iconEl.className = `resource-icon ${icon}`;
+        element.prepend(iconEl);
 
-      const resourceEl = element.nextElementSibling;
-      if (['audio', 'video'].includes(type) && (!resourceEl || resourceEl.tagName !== type.toUpperCase())) {
-        const mediaEl = document.createElement(type);
-        mediaEl.className = `media-player media-${type}`;
-        mediaEl.controls = true;
+        const type = mime.split('/')[1] === 'pdf' ? 'pdf' : mime.split('/')[0];
+        if (['audio', 'video', 'pdf'].includes(type)) {
+          const isPdf = type === 'pdf';
+          const globalName = isPdf ? 'pdfViewer' : `${type}Player`;
+          const isEnabled = await this.fetchGlobal<boolean>(`markdown.plugin.${globalName}`);
+          const resourceEl = element.nextElementSibling;
 
-        const sourceEl = document.createElement('source');
-        sourceEl.src = `${resourceUrl}.${file_extension}`;
-        sourceEl.type = mime;
+          if (isEnabled && (!resourceEl || resourceEl.tagName !== type.toUpperCase())) {
+            const mediaEl = document.createElement(isPdf ? 'object' : type);
+            mediaEl.className = `media-player media-${type}`;
 
-        mediaEl.appendChild(sourceEl);
-        element.after(mediaEl);
+            if (isPdf) {
+              mediaEl.data = `${resourceUrl}.${file_extension}`;
+              mediaEl.type = mime;
+            } else {
+              const sourceEl = document.createElement('source');
+              sourceEl.src = `${resourceUrl}.${file_extension}`;
+              sourceEl.type = mime;
+
+              mediaEl.controls = true;
+              mediaEl.appendChild(sourceEl);
+            }
+
+            element.after(mediaEl);
+          }
+        }
       }
     }
   }
